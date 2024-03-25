d1f874adffdb1f29096c4a2dd4627423188e5ffb
Siddharth U <138808939+siddharthyou29@users.noreply.github.com>
2024-03-25 11:00:20 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,97 @@
+import cv2
+import mediapipe as mp
+import os
+import time
+
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+
+# For webcam input:
+cap = cv2.VideoCapture(0)
+
+folderPath = "FingerImages"
+myList = os.listdir(folderPath)
+print(myList)
+overlayList = []
+for imPath in myList:
+    image = cv2.imread(f'{folderPath}/{imPath}')
+    # print(f'{folderPath}/{imPath}')
+    overlayList.append(image)
+
+pTime = 0
+with mp_hands.Hands(
+    model_complexity=0,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as hands:
+  while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+      print("Ignoring empty camera frame.")
+      continue
+
+
+    image.flags.writeable = False
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = hands.process(image)
+
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    fingerCount = 0
+
+    if results.multi_hand_landmarks:
+
+      for hand_landmarks in results.multi_hand_landmarks:
+
+        handIndex = results.multi_hand_landmarks.index(hand_landmarks)
+        handLabel = results.multi_handedness[handIndex].classification[0].label
+
+        handLandmarks = []
+
+        for landmarks in hand_landmarks.landmark:
+          handLandmarks.append([landmarks.x, landmarks.y])
+
+        
+        if handLabel == "Left" and handLandmarks[4][0] > handLandmarks[3][0]:
+          fingerCount = fingerCount+1
+        elif handLabel == "Right" and handLandmarks[4][0] < handLandmarks[3][0]:
+          fingerCount = fingerCount+1
+
+        
+        if handLandmarks[8][1] < handLandmarks[6][1]:       #Index finger
+          fingerCount = fingerCount+1
+        if handLandmarks[12][1] < handLandmarks[10][1]:     #Middle finger
+          fingerCount = fingerCount+1
+        if handLandmarks[16][1] < handLandmarks[14][1]:     #Ring finger
+          fingerCount = fingerCount+1
+        if handLandmarks[20][1] < handLandmarks[18][1]:     #Pinky
+          fingerCount = fingerCount+1
+
+        # Draw hand landmarks 
+        mp_drawing.draw_landmarks(
+            image,
+            hand_landmarks,
+            mp_hands.HAND_CONNECTIONS,
+            mp_drawing_styles.get_default_hand_landmarks_style(),
+            mp_drawing_styles.get_default_hand_connections_style())
+
+    # Display finger count
+    h, w, c = overlayList[fingerCount-1].shape
+    image[0:h, 0:w] = overlayList[fingerCount-1]
+
+    cv2.rectangle(image, (20, 225), (170, 425), (0, 255, 0), cv2.FILLED)
+    cv2.putText(image, str(fingerCount), (45, 375), cv2.FONT_HERSHEY_PLAIN,
+                10, (255, 0, 0), 25)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(image, f'FPS: {int(fps)}', (400, 70), cv2.FONT_HERSHEY_PLAIN,
+                3, (255, 0, 0), 3)
+    # Display image
+    cv2.imshow('MediaPipe Hands', image)
+    if cv2.waitKey(5) & 0xFF == 27:
+      break
+cap.release()
